cf5b3f5dc76a557686e47ed5030507dba63c0cd0
Szodfridt Gergely <szoti95@gmail.com>
2021-04-11 15:33:04 +0200

    About fragment was built in, navigation done

--- app/src/main/java/com/example/cocktaillist/ui/main/about/AboutViewModel.java
+++ app/src/main/java/com/example/cocktaillist/ui/main/about/AboutViewModel.java
@@ -1,4 +1,19 @@
 package com.example.cocktaillist.ui.main.about;
 
-public class AboutViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class AboutViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AboutViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is the about fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
